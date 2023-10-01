67c9ae8ce21ab72a917c167f207e1ea9e323e9b8
jademaggs <jademaggs@gmail.com>
2023-10-02 07:32:26 +1300

    Add length-weight and age-length data. Remove stonebream dataset.

---
+++ R/sbrm_fl_age.R
@@ -0,0 +1,20 @@
+#' Stonebream Length and Age Data
+#'
+#' Stonebream \emph{Neoscorpis lithophilus} length and age data.
+#'
+#' @docType data
+#'
+#' @usage data(sbrm_fl_age)
+#'
+#' @format Data in tabular format.
+#'
+#' @keywords datasets
+#'
+#' @references Data collected and owned by the Oceanographic Research Institute, Durban, South Africa.
+#'
+#' @source (\href{https://saambr.org.za/oceanographic-research-institute-ori/}{ORI})
+#'
+#' @examples
+#' library(jmsat)
+#' data(sbrm_fl_age)
+"sbrm_fl_age"
---
+++ R/sbrm_wgt_fl.R
@@ -0,0 +1,20 @@
+#' Stonebream Weight and Length Data
+#'
+#' Stonebream \emph{Neoscorpis lithophilus} weight and length data.
+#'
+#' @docType data
+#'
+#' @usage data(sbrm_wgt_fl)
+#'
+#' @format Data in tabular format.
+#'
+#' @keywords datasets
+#'
+#' @references Data collected and owned by the Oceanographic Research Institute, Durban, South Africa.
+#'
+#' @source (\href{https://saambr.org.za/oceanographic-research-institute-ori/}{ORI})
+#'
+#' @examples
+#' library(jmsat)
+#' data(sbrm_wgt_fl)
+"sbrm_wgt_fl"
